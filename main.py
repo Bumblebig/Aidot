d0e4724f7a501a32b071bf8390a169992c1bf811
Abbdurahman <dreydare16@gmail.com>
2024-07-17 14:08:26 +0100

    added error catcher for request retries

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
+import time
 from pinecone import Pinecone
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
@@ -21,7 +21,7 @@ pc = Pinecone(api_key=PINECONE_API_KEY)
 genai.configure(api_key=GOOGLE_API_KEY)
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for the Flask app
+CORS(app)
 
 def get_conversational_chain():
     prompt_template = """
@@ -49,18 +49,34 @@ def run_llm(query):
     model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=1)
     
     qa = RetrievalQA.from_chain_type(
-    llm=model,
-    chain_type="stuff",
-    retriever=docsearch.as_retriever())
-    
-    return qa.invoke(query)
+        llm=model,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever())
+
+    retries = 5
+    delay = 2  # Initial delay in seconds
+
+    for i in range(retries):
+        try:
+            response = qa.invoke(query)
+            if response and 'result' in response:
+                return response['result']
+            else:
+                return "No valid response found."
+        except Exception as e:
+            if 'ResourceExhausted' in str(e):
+                print(f"Retrying in {delay} seconds due to resource exhaustion...")
+                time.sleep(delay)
+                delay *= 2  # Exponential backoff
+            else:
+                raise e
+    return "Max retries exceeded. Please try again later."
 
 @app.route('/api/chatbot', methods=['POST'])
 def chatbot():
     user_message = request.json.get('message')
     response = run_llm(user_message)
-    return jsonify({'response': response['result']})
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
